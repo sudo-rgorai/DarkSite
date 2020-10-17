eff56260c52b1bc5842624a6a645eea26abda483
Rahul Gorai <rahul.gorai.3141@gmail.com>
2020-10-17 15:02:14 +0530

    Improve code formatting

--- index.js
+++ index.js
@@ -1,121 +1,111 @@
-window.onload=function()
-{
-    var url =window.location.href;
-    console.log(url);
-    var present=url.search("=");
-    console.log(present);
+window.onload = function () {
+  var url = window.location.href;
+  console.log(url);
+  var present = url.search("=");
+  console.log(present);
 
-    if(present!=-1)
-    {
-        var elements=url.split("=");
-        console.log(elements[1]);
-        document.cookie = "username="+elements[1]+";expires=Fri, 24 Apr 2030 00:00:00 UTC;";
-        this.document.cookie="";
-        console.log(this.document.cookie);
-    }
-    console.log(document.cookie);
-    if(document.cookie.length==0)
-    {
-        window.open("user.html","_parent","","");
-    }
-    else
-    {
-        var name=document.cookie.split("=")[1];
-        console.log(name);
-        var message="Hi "+name+"!";
-        console.log(message);
-        document.querySelector("h1").innerHTML=message;
-    }
-}
-function clock()
-{
-    var today = new Date();
-    var date=today.getDate();
-    if(date%10==1)
-        date=date+"st";
-    else if(date%10==2)
-        date=date+"nd";
-    else if(date%10==2)
-        date=date+"rd";
-    else
-        date=date+"th";
-var month = today.getMonth()+1;
-var monthWord;
-switch(month)
-{
+  if (present != -1) {
+    var elements = url.split("=");
+    console.log(elements[1]);
+    document.cookie =
+      "username=" + elements[1] + ";expires=Fri, 24 Apr 2030 00:00:00 UTC;";
+    this.document.cookie = "";
+    console.log(this.document.cookie);
+  }
+  console.log(document.cookie);
+  if (document.cookie.length == 0) {
+    window.open("user.html", "_parent", "", "");
+  } else {
+    var name = document.cookie.split("=")[1];
+    console.log(name);
+    var message = "Hi " + name + "!";
+    console.log(message);
+    document.querySelector("h1").innerHTML = message;
+  }
+};
+function clock() {
+  var today = new Date();
+  var date = today.getDate();
+  if (date % 10 == 1) date = date + "st";
+  else if (date % 10 == 2) date = date + "nd";
+  else if (date % 10 == 2) date = date + "rd";
+  else date = date + "th";
+  var month = today.getMonth() + 1;
+  var monthWord;
+  switch (month) {
     case 1:
-        monthWord="January";
-        break;
+      monthWord = "January";
+      break;
     case 2:
-        monthWord="February";
-        break;
+      monthWord = "February";
+      break;
     case 3:
-        monthWord="March";
-        break;
+      monthWord = "March";
+      break;
     case 4:
-        monthWord="April";
-        break;
+      monthWord = "April";
+      break;
     case 5:
-        monthWord="May";
-        break;
+      monthWord = "May";
+      break;
     case 6:
-        monthWord="June";
-        break;
+      monthWord = "June";
+      break;
     case 7:
-        monthWord="July";
-        break;
+      monthWord = "July";
+      break;
     case 8:
-        monthWord="August";
-        break;
+      monthWord = "August";
+      break;
     case 9:
-        monthWord="September";
-        break;
+      monthWord = "September";
+      break;
     case 10:
-        monthWord="October";
-        break;
+      monthWord = "October";
+      break;
     case 11:
-        monthWord="November";
-        break;
+      monthWord = "November";
+      break;
     case 12:
-        monthWord="December";
-        break;
-}
+      monthWord = "December";
+      break;
+  }
 
-var day =today.getDay();
-var dayWord;
-switch(day)
-{
+  var day = today.getDay();
+  var dayWord;
+  switch (day) {
     case 0:
-        dayWord="Sunday";
-        break;
+      dayWord = "Sunday";
+      break;
     case 1:
-        dayWord="Monday";
-        break;
+      dayWord = "Monday";
+      break;
     case 2:
-        dayWord="Tueday";
-        break;
+      dayWord = "Tuesday";
+      break;
     case 3:
-        dayWord="Wednesday";
-        break;
+      dayWord = "Wednesday";
+      break;
     case 4:
-        dayWord="Thursday";
-        break;
+      dayWord = "Thursday";
+      break;
     case 5:
-        dayWord="Friday";
-        break;
+      dayWord = "Friday";
+      break;
     case 6:
-        dayWord="Saturday";
-        break;
-}
+      dayWord = "Saturday";
+      break;
+  }
 
-    var date = date+' '+monthWord+' '+today.getFullYear();
-    
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = `${dayWord} &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp ${date} &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp ${time}`;
-    document.querySelector(".date").innerHTML=dateTime;
-    document.querySelector(".date").style.fontSize="1rem";
-    document.querySelector(".date").style.fontWeight="50";
-    document.querySelector(".date").style.marginTop="25px";
-    setTimeout(clock,1000);
+  var date = date + " " + monthWord + " " + today.getFullYear();
+
+  var time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = `${dayWord} &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp ${date} &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp ${time}`;
+  document.querySelector(".date").innerHTML = dateTime;
+  document.querySelector(".date").style.fontSize = "1rem";
+  document.querySelector(".date").style.fontWeight = "50";
+  document.querySelector(".date").style.marginTop = "25px";
+  setTimeout(clock, 1000);
 }
 clock();
