36b11393fd57240d961b330e42820b7f3f4a86a7
anushkasinghal11 <you@example.com>
2020-12-07 20:30:49 +0530

    converted string concatenation to template literals and var to let and const

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 window.onload = function() {
-  var url = window.location.href;
-  var present = url.search("=");
+  const url = window.location.href;
+  const present = url.search("=");
 
   if (present != -1) {
-    var elements = url.split("=");
-    document.cookie = "username=" + elements[1] + ";expires=Fri, 24 Apr 2030 00:00:00 UTC;";
+    const elements = url.split("=");
+    document.cookie = `username=${elements[1]};expires=Fri, 24 Apr 2030 00:00:00 UTC;`;
     this.document.cookie = "";
   }
   if (document.cookie.length == 0) {
     window.open("user.html", "_parent", "", "");
   } else {
-    var name = document.cookie.split("=")[1];
-    var myDate = new Date();
-    var hrs = myDate.getHours();
-    var message;
+    const name = document.cookie.split("=")[1];
+    const myDate = new Date();
+    const hrs = myDate.getHours();
+    let message;
     if (hrs >= 2 && hrs < 12)
-      message = 'Good Morning, ' + name + '!';
+      message = `Good Morning, ${name}!`;
     else if (hrs >= 12 && hrs <= 17)
-      message = 'Good Afternoon, ' + name + '!';
+      message = `Good Afternoon, ${name}!`;
     else if (hrs >= 17 && hrs <= 20)
-      message = 'Good Evening, ' + name + '!';
+      message = `Good Evening, ${name}!`;
     else
-      message = 'Welcome Back, ' + name + '!';
+      message = `Welcome Back, ${name}!`;
 
     document.getElementById("greet").innerHTML = message;
   }
@@ -29,24 +29,24 @@ window.onload = function() {
   fetch('quotes.txt')
   .then(response => response.text())
   .then(data => {
-    var res = data.split("\n");
+    const res = data.split("\n");
     document.getElementById('quote').textContent = res[Math.floor((Math.random()*50)+1)];
   });
 };
 
 function clock() {
-  var today = new Date();
+  const today = new Date();
   var date = today.getDate();
   if (date % 10 == 1)
-    date = date + "st";
+    date = `${date}st`;
   else if (date % 10 == 2)
-    date = date + "nd";
+    date = `${date}nd`;
   else if (date % 10 == 3)
-    date = date + "rd";
+    date = `${date}rd`;
   else
-    date = date + "th";
-  var month = today.getMonth() + 1;
-  var monthWord;
+    date = `${date}th`;
+  const month = today.getMonth() + 1;
+  let monthWord;
   switch (month) {
     case 1:
       monthWord = "January";
@@ -86,8 +86,8 @@ function clock() {
       break;
   }
 
-  var day = today.getDay();
-  var dayWord;
+  const day = today.getDay();
+  let dayWord;
   switch (day) {
     case 0:
       dayWord = "Sunday";
@@ -112,10 +112,10 @@ function clock() {
       break;
   }
 
-  var date = date + " " + monthWord + " " + today.getFullYear();
+  var date = `${date} ${monthWord} ${today.getFullYear()}`;
 
-  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  var dateTime = `${dayWord} &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp ${date} &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp ${time}`;
+  const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+  const dateTime = `${dayWord} &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp ${date} &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp ${time}`;
   document.querySelector(".date").innerHTML = dateTime;
   document.querySelector(".date").style.fontSize = "1rem";
   document.querySelector(".date").style.fontWeight = "50";
