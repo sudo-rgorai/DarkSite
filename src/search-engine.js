7f269959a31719211a96e4679c0cbdfe21f5199b
Harshit Kumar <kumarharshit850@gmail.com>
2021-01-03 23:52:10 +0530

    Search Engine added

--- src/search-engine.js
+++ src/search-engine.js
@@ -2,8 +2,20 @@ var selectedEngine = "";
 
 $(document).ready(function(){
   $("select.search-engine").change(function(){
-      selectedEngine = $(this).children("option:selected").val();
-      console.log("Searching on " + selectedEngine);
+      selectedEngine = ($(this).children("option:selected").val());
+      const engine = selectedEngine.toLocaleLowerCase();
+      let action = "";
+      if(engine === "yahoo") {
+        action = "http://www.search.yahoo.com/search";
+      }
+      else if(engine === "qwant") {
+        action = "http://www." + engine + ".com/";
+      }
+      else {
+        action = "http://www." + engine + ".com/search";
+      }
+      document.getElementById("search-form").action = action;
+      console.log(action);
   });
 });
 
